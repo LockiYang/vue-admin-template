a1e6bce6c5de966268f8b96102a0bdf41b86e5de
Locki <Locki>
2022-06-01 18:10:50 +0800

    fix

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Admin Template'
+const title = defaultSettings.siteTitle || 'Vue Admin Template'
 
 export default function getPageTitle (pageTitle) {
   if (pageTitle) {
